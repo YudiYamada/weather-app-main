705cf8ea0c8969844ac027c1a5898729b5e1b13e
Yudi Yamada <yudiyamada10@gmail.com>
2026-02-08 18:25:44 -0300

    refactor: change tag

--- src/components/card-atmospheric-data.tsx
+++ src/components/card-atmospheric-data.tsx
@@ -6,7 +6,7 @@ interface AtmosphericDataProps {
 const CardAtmosphericData = ({ title, value }: AtmosphericDataProps) => {
   return (
     <div className="boder rounded-xl bg-neutral-600 p-7">
-      <h3 className="mb-4 text-neutral-200">{title}</h3>
+      <span className="mb-4 text-neutral-200">{title}</span>
       <span className="text-4xl">{value}</span>
     </div>
   );
