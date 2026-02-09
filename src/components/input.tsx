e726e100e1b1fb77de90d72b1268396e937de9bd
Yudi Yamada <yudiyamada10@gmail.com>
2026-02-09 18:36:14 -0300

    feat: add sugestion on input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,10 +1,17 @@
-interface InputProps {
+import { useState } from "react";
+import DropdownGroup from "./dropdown-group";
+import DropdownItem from "./dropdown-item";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder: string;
   type: string;
   icon?: string;
+  suggestions?: string[];
 }
 
-const Input = ({ placeholder, type, icon }: InputProps) => {
+const Input = ({ placeholder, type, icon, suggestions = [], ...rest }: InputProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="relative w-full">
       {icon && (
@@ -18,7 +25,25 @@ const Input = ({ placeholder, type, icon }: InputProps) => {
         type={type}
         placeholder={placeholder}
         className={`w-full rounded-md bg-neutral-600 p-4 ${icon ? "pl-12" : "pl-4"}`}
+        onFocus={() => setOpen(true)}
+        onBlur={() => setTimeout(() => setOpen(false), 200)}
+        {...rest}
       />
+
+      {open && suggestions.length > 0 && (
+        <div className="absolute z-50 mt-2 w-full rounded-xl border border-neutral-700 bg-neutral-800 p-2 shadow-2xl">
+          <DropdownGroup title="Suggestions">
+            {suggestions.map((city) => (
+              <DropdownItem
+                key={city}
+                label={city}
+                isActive={false}
+                onClick={() => {}}
+              />
+            ))}
+          </DropdownGroup>
+        </div>
+      )}
     </div>
   );
 };
