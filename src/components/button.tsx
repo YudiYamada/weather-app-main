d4ad11c56b922b283490856d14c07886d707fa68
Yudi Yamada <yudiyamada10@gmail.com>
2026-02-09 18:33:11 -0300

    feat: add html attributes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,17 @@
-interface ButtonProps {
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button = ({ children }: ButtonProps) => {
-  return <button className="rounded-md bg-blue-500 p-4 hover:cursor-pointer">{children}</button>;
+const Button = ({ children, ...rest }: ButtonProps) => {
+  return (
+    <button
+      className="rounded-md bg-blue-500 p-4 hover:cursor-pointer"
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
